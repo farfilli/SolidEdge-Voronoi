ee6b9cbbb2866fbd3849f9583aec2170935d89d7
Francesco <francesco.arfilli@gmail.com>
2023-11-30 10:28:18 +0100

    Update AssemblyInfo.vb

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' usando l'asterisco '*' come illustrato di seguito:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.2")>
-<Assembly: AssemblyFileVersion("0.2")>
+<Assembly: AssemblyVersion("0.3")>
+<Assembly: AssemblyFileVersion("0.3")>
